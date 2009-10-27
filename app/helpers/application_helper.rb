74510d61755593f52365eac044e3e7895852c11c
Timur Vafin <timur.vafin@flatsoft.com>
2009-10-27 20:47:35 +0300

    Added url for phrase

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,31 @@ module ApplicationHelper
     APP_CONFIG['app_name']
   end
 
+  def clippy(text, bgcolor='#FFFFFF')
+    <<-EOF
+    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+            width="110"
+            height="14"
+            id="clippy" >
+    <param name="movie" value="/images/clippy.swf"/>
+    <param name="allowScriptAccess" value="always" />
+    <param name="quality" value="high" />
+    <param name="scale" value="noscale" />
+    <param NAME="FlashVars" value="text=#{text}">
+    <param name="bgcolor" value="#{bgcolor}">
+    <embed src="/images/clippy.swf"
+           width="110"
+           height="14"
+           name="clippy"
+           quality="high"
+           allowScriptAccess="always"
+           type="application/x-shockwave-flash"
+           pluginspage="http://www.macromedia.com/go/getflashplayer"
+           FlashVars="text=#{text}"
+           bgcolor="#{bgcolor}"
+    />
+    </object>
+    EOF
+  end
+
 end
